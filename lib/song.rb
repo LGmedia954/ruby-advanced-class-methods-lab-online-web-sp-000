356a005a4c9d86f06e7e355bf5c5e443152ef2f9
Leslie G <lesliegonzalez@me.com>
2020-08-03 23:44:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,11 +49,23 @@ end
 
 def self.new_from_filename(filename)
   
+  
+  
+  song = self.create
+  song.name = song_name
+  song.artist_name = artist_name
+  song
 end
 
 
 def self.create_from_filename(filename)
   
+  
+  
+  song = self.create
+  song.name = song_name
+  song.artist_name = artist_name
+  song
 end
 
 
